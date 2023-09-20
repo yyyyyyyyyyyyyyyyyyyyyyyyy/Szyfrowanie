1535a8e6eafeb772ae68ce392d149c0a4e42aad3
przybylski.pawel <przybylski.pawel@STACJA105-08>
2023-09-20 13:42:03 +0200

    Dodaj pliki projektów.

---
+++ Szyfrowanie/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Szyfrowanie;
+using static Szyfrowanie.szyfr;
+
+
+
+namespace Szyfrowanie
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Przycisk_Click(object sender, RoutedEventArgs e)
+        {
+            string text = TextInput.Text;
+            int klucz = 0;
+
+
+
+            if (!int.TryParse(KeyInput.Text, out klucz))
+            {
+                MessageBox.Show("Klucz musi być liczbą całkowitą.");
+                return;
+            }
+            if (Szyfr.IsChecked == true)
+            {
+                Wynik.Text = Szyfring(text, klucz);
+            }
+            else if (OdSzyfr.IsChecked == true)
+            {
+                Wynik.Text = NieSzyfring(text, klucz);
+            }
+        }
+    }
+}
---
+++ Szyfrowanie/szyfr.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Szyfrowanie
+{
+    class szyfr
+    {
+
+
+        public static string Szyfring(string text, int key)
+        {
+            char[] result = new char[text.Length];
+
+
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char ch = text[i];
+                if (char.IsLetter(ch))
+                {
+                    char baseChar = char.IsLower(ch) ? 'a' : 'A';
+                    result[i] = (char)(((ch - baseChar + key) % 26 + 26) % 26 + baseChar);
+                }
+                else
+                {
+                    result[i] = ch;
+                }
+            }
+
+
+
+            return new string(result);
+        }
+
+
+
+        public static string NieSzyfring(string text, int key)
+        {
+            return Szyfring(text, 26 - key);
+        }
+    }
+}
